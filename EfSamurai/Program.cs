e81a2cbaf082a23c19d5b67e22adb0bfdf8965fa
Unknown <mattiasj@protonmail.com>
2018-04-17 14:50:33 +0200

    Last known good

--- EfSamurai/Program.cs
+++ EfSamurai/Program.cs
@@ -9,9 +9,49 @@ namespace EfSamurai
     {
         static void Main(string[] args)
         {
-            AddSamurais();
+            // AddSamurais();
+
+            AddBattles();
+
             Console.WriteLine("Allt är kört");
             Console.ReadKey();
+        }
+
+        private static void AddBattles()
+        {
+            var battle = new Battle()
+            {
+                Name = "Tokyo",
+                Brutal = true,
+                Battlelog = new Battlelog(),
+                Start = DateTime.Now,
+                End = DateTime.Now.AddDays(1)
+            };
+
+            var battle1 = new Battle()
+            {
+                Name = "Kyoto",
+                Brutal = false,
+                Battlelog = new Battlelog(),
+                Start = DateTime.Now,
+                End = DateTime.Now.AddDays(14)
+            };
+
+            var battle2 = new Battle()
+            {
+                Name = "Hiroshima",
+                Brutal = true,
+                Battlelog = new Battlelog(),
+                Start = DateTime.Now,
+                End = DateTime.Now.AddDays(14)
+            };
+
+
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.AddRange(battle, battle1, battle2);
+                context.SaveChanges();
+            }
         }
 
         private static void AddSamurais()
@@ -22,25 +62,25 @@ namespace EfSamurai
                 Quote = new Quote() {Text = "Citatet", Type = Awesome },
                 Hairstyle = HairStyle.Chonmage,
                 Battle = new Battle(),
-                Identity = new SecretIdentity()
+                Identity = new SecretIdentity() { Name = "Perciwall" },
             };
 
             var samurai1 = new Samurai
             {
                 Name = "Quiff",
-                Quote = new Quote() { Text = "Citatet", Type = Awesome },
-                Hairstyle = HairStyle.Chonmage,
+                Quote = new Quote() { Text = "Citatet", Type = Cheesy },
+                Hairstyle = HairStyle.Western,
                 Battle = new Battle(),
-                Identity = new SecretIdentity()
+                Identity = new SecretIdentity() { Name = "Frank" },
             };
 
             var samurai2 = new Samurai
             {
                 Name = "Databas",
-                Quote = new Quote() { Text = "Citatet", Type = Awesome },
-                Hairstyle = HairStyle.Chonmage,
-                Battle = new Battle(),
-                Identity = new SecretIdentity()
+                Quote = new Quote() { Text = "Citatet", Type = Lame },
+                Hairstyle = HairStyle.Oicho,
+                Identity = new SecretIdentity() { Name = "Kasaban" },
+                Battle = new Battle()
             };
 
 
